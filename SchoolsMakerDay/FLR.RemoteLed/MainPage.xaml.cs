4a0a5cf63c816d1ac448258d9d06e3ce581d3c94
Nicola Amadei <nicola.amadei@gmail.com>
2016-04-29 10:11:42 +0200

    Added virtual-only mode in RemoteLED
    Now app can be used also on a non-raspberry system. Added virtual LEDs.

--- SchoolsMakerDay/FLR.RemoteLed/MainPage.xaml.cs
+++ SchoolsMakerDay/FLR.RemoteLed/MainPage.xaml.cs
@@ -39,21 +39,30 @@ namespace FLR.RemoteLed
         public ILed LedR { get; set; }
         public ILed LedG { get; set; }
         public ILed LedV { get; set; }
+        public bool HasGPIO { get; set; }
+        private SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
+        private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+        private SolidColorBrush yellowBrush = new SolidColorBrush(Windows.UI.Colors.Yellow);
+        private SolidColorBrush greenBrush = new SolidColorBrush(Windows.UI.Colors.Green);
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            var controller = GpioController.GetDefault();
-            if (controller != null)
-            {                
-                Messenger = new Pubnub("pub-c-6133a45b-d0a7-48d7-a1a8-ca611ee0826e", "sub-c-b0c87e72-0af3-11e6-996b-0619f8945a4f");
-                Messenger.Subscribe<string>("flr_remoteled", userCallBack, connectCallback, errorCallback);
+            var GPIO = GpioController.GetDefault();
+            if (GPIO != null)
+            {
+                HasGPIO = true;                 
                 GrovePi = DeviceFactory.Build;
                 LedR = GrovePi.Led(Pin.DigitalPin2).ChangeState(SensorStatus.Off);
                 LedG = GrovePi.Led(Pin.DigitalPin3).ChangeState(SensorStatus.Off);
-                LedV = GrovePi.Led(Pin.DigitalPin4).ChangeState(SensorStatus.Off);            
+                LedV = GrovePi.Led(Pin.DigitalPin4).ChangeState(SensorStatus.Off);
+                lblStatus.Text = "Ho trovato un controller!";      
             }
             else
-                lblTesto.Text = "No GPIO Controller";
+            {
+                lblStatus.Text = "Nessun controller!";
+            }
+            Messenger = new Pubnub("pub-c-6133a45b-d0a7-48d7-a1a8-ca611ee0826e", "sub-c-b0c87e72-0af3-11e6-996b-0619f8945a4f");
+            Messenger.Subscribe<string>("flr_remoteled", userCallBack, connectCallback, errorCallback);
         }
 
         private async void userCallBack(string obj)
@@ -71,25 +80,49 @@ namespace FLR.RemoteLed
                             case "Rosso":
                                 {
                                     if (msg.Stato)
-                                        LedR.ChangeState(SensorStatus.On);
+                                    {
+                                        if(HasGPIO)
+                                            LedR.ChangeState(SensorStatus.On);
+                                        epsLedR.Fill = redBrush;
+                                    }
                                     else
-                                        LedR.ChangeState(SensorStatus.Off);
+                                    {
+                                        if(HasGPIO)
+                                            LedR.ChangeState(SensorStatus.Off);
+                                        epsLedR.Fill = grayBrush;
+                                    }
                                     break;
                                 }
                             case "Giallo":
                                 {
                                     if (msg.Stato)
-                                        LedG.ChangeState(SensorStatus.On);
+                                    {
+                                        if(HasGPIO)
+                                            LedG.ChangeState(SensorStatus.On);
+                                        epsLedG.Fill = yellowBrush;
+                                    }
                                     else
-                                        LedG.ChangeState(SensorStatus.Off);
+                                    {
+                                        if(HasGPIO)
+                                            LedG.ChangeState(SensorStatus.Off);
+                                        epsLedG.Fill = grayBrush;
+                                    }
                                     break;
                                 }
                             case "Verde":
                                 {
                                     if (msg.Stato)
-                                        LedV.ChangeState(SensorStatus.On);
+                                    {
+                                        if(HasGPIO)
+                                            LedV.ChangeState(SensorStatus.On);
+                                        epsLedV.Fill = greenBrush;
+                                    }
                                     else
-                                        LedV.ChangeState(SensorStatus.Off);
+                                    {
+                                        if(HasGPIO)
+                                            LedV.ChangeState(SensorStatus.Off);
+                                        epsLedV.Fill = grayBrush;
+                                    }
                                     break;
                                 }
                         }
@@ -98,7 +131,7 @@ namespace FLR.RemoteLed
             }
             catch
             {
-                lblTesto.Text = "Error detected at: " + DateTime.Now.Minute + DateTime.Now.Second;
+                lblStatus.Text = "Fail detected";
             }
         }
 
